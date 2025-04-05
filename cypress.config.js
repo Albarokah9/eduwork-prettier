6b541529d56cace2765a3f86a783a1a92597d660
Albarokah Rifansah <albareizy942@gmail.com>
2025-04-05 21:15:55 +0700

    update-file

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://zero.webappsecurity.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Pastikan pola ini mencakup file Anda
   },
 });
---
+++ cypress/e2e/learning-cypress-advance/seacrhbox.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+// Cypress Test Case: Searchbox Test
+
+describe('Searcbox Test', () => {
+   beforeEach(() => {
+      cy.visit('/index.html')
+   });
+   
+   it('Should type into searchbox and submit', () => {
+    cy.get('#searchTerm', { timeout: 10000 }).type('some text {enter}')
+    cy.url().should('include', 'search.html')
+   });
+
+   it('Should show search result page', () => {
+      cy.get('#searchTerm', { timeout: 10000 }).type('some text {enter}')
+      cy.url().should('include', 'search.html')
+      cy.get('h2').should('contain.text', 'Search Results:') 
+   });
+}); 
+
+// Atribut	Format Cypress	Contoh
+// id=	    #idName	        cy.get('#user-name')
+// class=	  .className	    cy.get('.btn-primary')
+// type=	  [type="value"]	cy.get('[type="text"]')
+
+// Atribut lain	[atribut="value"]	cy.get('[data-test="login-button"]')
+// Gabungan	tagname#idName.className[type="value"]	cy.get('input#user-name[type="text"]')
+// Gabungan lain	tagname.className#idName[type="value"]	cy.get('input.btn-primary#user-name[type="text"]')
