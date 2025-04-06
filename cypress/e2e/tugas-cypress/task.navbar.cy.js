42a35810f82077dc891c2ba4b13649f422e4abbb
Albarokah Rifansah <albareizy942@gmail.com>
2025-04-06 18:49:55 +0700

    update-file

--- cypress/e2e/tugas-cypress/task.navbar.cy.js
+++ cypress/e2e/tugas-cypress/task.navbar.cy.js
@@ -26,7 +26,8 @@ describe('Navbar test', () => {
      cy.contains('Zero Bank').click();
      cy.url().should('include', 'index.html');
      cy.get('h4').should('contain.text', 'Online Banking');
-     
+     cy.get('p').should('contain.text', 'Welcome to Zero Online Banking. Zero provides a greener and more convenient way to manage your money. Zero enables you to check your account balances, pay your bills, transfer money, and  keep detailed records of your transactions,  wherever there is an internet connection.')
+
     });
  
  });
