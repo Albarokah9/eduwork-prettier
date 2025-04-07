8e7cbecbd350c7629c19e8e926b9eeb29afa77d3
Albarokah Rifansah <albareizy942@gmail.com>
2025-04-07 18:12:52 +0700

    update

--- cypress/e2e/tugas-cypress/task-project-e2e-all-proces-in-web/logout.cy.js
+++ cypress/e2e/tugas-cypress/task-project-e2e-all-proces-in-web/logout.cy.js
@@ -5,19 +5,21 @@ describe('Logout', () => {
 		// Kunjungi halaman login
 		// Sebaiknya login terlebih dahulu sebelum melakukan logout
 		cy.visit('https://www.saucedemo.com/')
-		// Login menggunakan kredensial valid dengan menggunakan fi
-		cy.fixture('saucedemo').then(data => {
-			const username = data.validUser.username
-			const password = data.validUser.password
+		// // Login menggunakan kredensial valid dengan menggunakan fi
+		// cy.fixture('saucedemo').then(data => {
+		// 	const username = data.validUser.username
+		// 	const password = data.validUser.password
 
-			cy.get('#user-name').type(username)
-			cy.get('#password').type(password)
-			cy.contains('Login').click()
+		// 	cy.get('#user-name').type(username)
+		// 	cy.get('#password').type(password)
+		// 	cy.contains('Login').click()
 
+        // Login menggunakan custom command dengan menggunakan fixture
+        cy.loginWithFixture2('validUser')
 			// Assertion: Memastikan berhasil login
 			cy.url().should('include', '/inventory.html')
 			cy.get('.title').should('contain', 'Products')
-		})
+		// })
 	})
 
 	it('Logout dari akun menggunakan costume commands', () => {
