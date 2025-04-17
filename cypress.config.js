b4f5d1848b52ab1ecb2e60a52fe3cdf5690c826d
Albarokah Rifansah <albareizy942@gmail.com>
2025-04-17 11:22:21 +0700

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,27 @@
-const { defineConfig } = require('cypress')
+// const { defineConfig } = require('cypress')
+
+// module.exports = defineConfig({
+// 	e2e: {
+// 		// baseUrl: 'http://zero.webappsecurity.com',
+// 		setupNodeEvents(on, config) {
+// 			// implement node event listeners here
+// 		},
+// 		specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Pastikan pola ini mencakup file Anda
+// 	},
+// })
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
 	e2e: {
-		// baseUrl: 'http://zero.webappsecurity.com',
-		setupNodeEvents(on, config) {
-			// implement node event listeners here
+		baseUrl: 'https://reqres.in',
+		env: {
+			userEmail: "eve.holt@reqres.in",
+			userPassword: "cityslicka",
+			apiUrl: 'https://reqres.in/api/login'
 		},
-		specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Pastikan pola ini mencakup file Anda
-	},
-})
+		setupNodeEvents(on, config) {
+			// Implementasi event di sini
+		},
+		specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}' // Pastikan pola ini mencakup file tes kamu
+	}
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -121,3 +121,19 @@ Cypress.Commands.add('checkout', (firstName, lastName, postalCode) => {
 		)
 	})
 })
+
+Cypress.Commands.add('loginViaAPI',(
+	email = Cypress.env('userEmail'),
+	password = Cypress.env('userPassword'),
+) => {
+	cy.request('POST', `${Cypress.env('apiUrl')}/users/login`, {
+		username: email,
+		password,
+	}).then((response) => {
+		cy.setCookie('sessionId', response.body.sessionId)
+		cy.setCookie('userId', response.body.userId)
+		cy.setCookie('userName', response.body.userName)
+		cy.visit('/#!/main')
+	})
+})
+
